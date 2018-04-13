6ea132ea74b1045b8b37b28c499e43225c28db2b
J Ho XRay <jho.xray@gmail.com>
2018-04-14 00:50:35 +0200

    made multitree instance of functor, monoid, foldable, traversable

--- src/PicoGUI/Abstract.hs
+++ src/PicoGUI/Abstract.hs
@@ -1,7 +1,35 @@
 module PicoGUI.Abstract where
 
+-- import Data.Monoid hiding ((<>))
+
 data MultiTree a = Node a [MultiTree a] deriving Show
 
+instance Monoid a => Monoid (MultiTree a) where 
+    (Node x xs) `mappend` (Node y ys) = Node (x `mappend` y) (xs ++ ys) 
+    mempty = Node mempty []
+
+instance Functor MultiTree where
+    fmap f (Node x []) = Node (f x) []
+    fmap f (Node x tr) = Node (f x) (map (fmap f) tr)
+
+instance Foldable MultiTree where
+    -- foldMap :: Monoid m => (a -> m) -> t a -> m
+    -- foldr :: (a -> b -> b) -> b -> t a -> b
+    foldMap f (Node x []) = f x
+    foldMap f (Node x ts) = f x `mappend` mconcat (map (foldMap f) ts)
+
+instance Traversable MultiTree where 
+    -- traverse :: Applicative f => (a -> f b) -> t a -> f (t b)
+    traverse f (Node x []) = Node <$> f x <*> pure []
+    traverse f (Node x ls) = Node <$> f x <*> traverse (traverse f) ls
+
+{-
+-- identity and homomorphism work, check other laws!!!
+instance Applicative MultiTree where
+    pure x = Node x []
+    (Node f []) <*> tree = fmap f tree
+    -- (Node f (g:gs)) <*> tree = ???
+-}
 
 empty :: a -> MultiTree a
 empty el = Node el []
@@ -34,7 +62,7 @@ infixr 8 +>>
 
 t2 = empty 0 +>> 
         empty 1 ->> empty 2 ->> empty 3 ->> empty 4 
-        +>>
+        ->>
         empty 10
 
 t3 = empty 0 +>> empty 1 +>> empty 2 +>> empty 3
